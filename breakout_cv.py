4cfceb9e7b26ee7cbc12be222265a06e73c4305a
Saraiv <mainsaraiva@gmail.com>
2022-12-12 15:49:55 +0000

    Fase 3 feita

--- breakout_cv.py
+++ breakout_cv.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
+import yolov5
 
 bg = cv2.createBackgroundSubtractorMOG2()
+model = yolov5.load('../yolov5n.pt')
+model.conf = 0.33
 
 def cv_setup(game):
     cv_init(game)
@@ -19,7 +22,8 @@ def cv_update(game):
     ret, image = cap.read()
     image = image[:, ::-1, :]
     # cv_process(game, image)
-    cv_process_second(game, image)
+    # cv_process_second(game, image)
+    cv_process_third(game, image)
     # cv_output(image)
     game.after(1, cv_update, game)
 
@@ -83,6 +87,48 @@ def cv_process_second(game, image):
         game.paddle.move(-10)
     else:
         game.paddle.move(0)
+
+def cv_process_third(game, image):
+    imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    results = model(imageRGB)
+    output = image.copy()
+
+    for pred in enumerate(results.pred):
+        im = pred[0]
+        im_boxes = pred[1]
+        for *box, conf, cls in im_boxes:
+            box_class = int(cls)
+            conf = float(conf)
+            x = float(box[0])
+            y = float(box[1])
+            w = float(box[2]) - x
+            h = float(box[3]) - y
+            pt1 = np.array(np.round((float(box[0]), float(box[1]))), dtype=int)
+            pt2 = np.array(np.round((float(box[2]), float(box[3]))), dtype=int)
+            box_color = (255, 0, 0)
+            if results.names[box_class] == 'cell phone':
+                text = "{}:{:.2f}".format(results.names[box_class], conf)
+                cv2.rectangle(img=output,
+                              pt1=pt1,
+                              pt2=pt2,
+                              color=box_color,
+                              thickness=1)
+                cv2.putText(img=output,
+                            text=text,
+                            org=np.array(np.round((float(box[0]), float(box[1] - 1))), dtype=int),
+                            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                            fontScale=0.5,
+                            color=box_color,
+                            thickness=1)
+
+                if x > 250:
+                    game.paddle.move(10)
+                elif x < 250:
+                    game.paddle.move(-10)
+                else:
+                    game.paddle.move(0)
+
+    cv2.imshow("YOLOv5", output)
 
 def cv_output(image):
     cv2.imshow("Image", image)
